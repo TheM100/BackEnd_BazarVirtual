d39edd3851f995d260cdb4b2957d74d6ad3a9047
TheM100 <carlosgastelum9923@gmail.com>
2024-08-18 21:34:24 -0600

    add some changes

--- src/middlewares/bazarMiddle.js
+++ src/middlewares/bazarMiddle.js
@@ -1,13 +1,29 @@
 const validateDate = (req, res, next) => {
-    const newDate = req.body;
-    const currentDate = new Date();
+  //   const newDate = req.body;
+  //   const currentDate = new Date();
     
+  // console.log(newDate)
+  // console.log(currentDate)
   
-    if (new Date(newDate.date) < currentDate) {
-      return res.status(400).json({ msg: "No se puede crear una fecha en el pasado" });
-    }
+  //   if (new Date(newDate.date) < currentDate) {
+  //     return res.status(400).json({ msg: "No se puede crear una fecha en el pasado" });
+  //   }
+
+  const inputDate = new Date(req.body.date);
+
+  // Obtener la fecha actual y ajustarla a medianoche (hora local)
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+
+  // Ajustar la fecha proporcionada para la zona horaria local
+  const localDate = new Date(inputDate.toLocaleDateString());
   
-    next(); // Continuar con el siguiente middleware o controlador
+  // Comprobar si la fecha proporcionada está en el pasado
+  if (localDate < currentDate) {
+    return res.status(400).json({ msg: "No se puede crear una fecha en el pasado" });
+  }
+  
+    next(); 
   };
 
   module.exports = validateDate;
