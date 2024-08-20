4cdb6eb730728b3d3673d75969a513bb3f918471
geranhuns <gera.nuncio@gmail.com>
2024-08-20 17:09:08 -0600

    brought stripe from frontend

--- src/Routes/ordersRoutes.js
+++ src/Routes/ordersRoutes.js
@@ -5,11 +5,15 @@ const {
   getFromClient,
   getFromBrand,
   deliveredTrue,
+  newPaymentIntent,
+  getPaymentIntent,
 } = require("../controllers/ordersControllers");
 
 router.get("/clientOrders/:clientId", getFromClient);
 router.get("/brandSales/:brandId", getFromBrand);
+router.get("/get-payment-intent", getPaymentIntent);
 router.post("/newOrder", postNewPurchase);
+router.post("/create-payment-intent", newPaymentIntent);
 router.put("/deliveredProduct", deliveredTrue);
 
 module.exports = router;
