f88e886feaf87fc50287ab33311e2fba768c0d68
geranhuns <gera.nuncio@gmail.com>
2024-08-28 23:57:49 -0600

    updated bazarModel

--- src/models/bazar/newDateBazar.js
+++ src/models/bazar/newDateBazar.js
@@ -1,42 +1,38 @@
 const mongooselib = require("mongoose");
-const especialEventsSchema = require('./schemaAux/especialEvents');
-const marcasCursoSchema = require('./schemaAux/marcasCurso')
+const especialEventsSchema = require("./schemaAux/especialEvents");
+const marcasCursoSchema = require("./schemaAux/marcasCurso");
 
 const newDateBazarSchema = new mongooselib.Schema({
-createdBy: {
-    type:String,
+  createdBy: {
+    type: String,
     type: mongooselib.Schema.Types.ObjectId,
-        ref: 'usersBazar',
-        required: true
-    },
+    ref: "usersBazar",
+    required: true,
+  },
   place: {
     type: String,
     required: true,
-    
   },
   date: {
     type: Date,
-    required: true,  
-   
+    required: true,
   },
   time: {
     type: String,
     required: true,
-   
   },
   marcasCurso: {
     type: [marcasCursoSchema],
-    
-  
-   
   },
   events: {
     type: [especialEventsSchema],
-    required: true,
-    
   },
 });
 
-const dateBazarModel = mongooselib.model("newDateBazar", newDateBazarSchema, "datesBazares"); //primer parametro:nombre_modelo, segundo parametro: nombre_esquema_a_utilizar, tercer parametro: nombre-de-coleccion-en-la-BD
+const dateBazarModel = mongooselib.model(
+  "newDateBazar",
+  newDateBazarSchema,
+  "datesBazares"
+); //primer parametro:nombre_modelo, segundo parametro: nombre_esquema_a_utilizar, tercer parametro: nombre-de-coleccion-en-la-BD
 
 module.exports = dateBazarModel;
